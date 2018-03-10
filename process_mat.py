34120e16996e2989bb50b3083f853cf352eab4bc
Cédric Lemaitre <c.lemaitre58@gmail.com>
2018-03-10 22:41:37 +0100

    ajout process list fichier matt

--- process_mat.py
+++ process_mat.py
@@ -2,13 +2,19 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 import numpy as np
 
-Cx = []
-df = pd.read_csv('./forceCoeff_grossier.csv')
-df2 = pd.read_csv('./forceCoeef_fin.csv')
+name_file = ["forceCoeffs_25_3.csv", "forceCoeffs_34_55.csv",
+             "forceCoeff_30_55.csv"]
 
-Cx.append(df['Cd'])
-Cx.append(df2['Cd'])
+path_dir = "/home/cedric/Documents/Ergocycle/data/DataEstACd/20180305/"
+
+Cx = []
+
+for fichier in name_file:
+
+    df = pd.read_csv(path_dir + fichier)
+    Cx.append([df['Cd'], fichier])
+
 
 for elt_cx in Cx:
-    elt_cx = np.array(elt_cx)
-    print(np.mean(elt_cx[75:]))
+    data_cx = np.array(elt_cx[0])
+    print(elt_cx[1], " : ", np.mean(data_cx[75:]))
